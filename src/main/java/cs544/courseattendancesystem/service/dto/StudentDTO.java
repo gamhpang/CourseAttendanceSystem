9b909ac54d8356ef5f3ed33707fad248937e447d
Nwai Thingyan <nwaithingyan485@gmail.com>
2024-06-15 16:42:47 -0500

    Add constructor

--- src/main/java/cs544/courseattendancesystem/service/dto/StudentDTO.java
+++ src/main/java/cs544/courseattendancesystem/service/dto/StudentDTO.java
@@ -1,4 +1,30 @@
 package cs544.courseattendancesystem.service.dto;
 
-public class StudentDTO {
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+public class StudentDTO extends PersonDTO{
+    //    private Long id;
+    private String entry;
+    //    private Faculty faculty;
+    private Long facultyId;
+    private long alternateId;
+    private long applicantId;
+    private long studentId;
+    private String barCode;
+
+    public StudentDTO(){}
+
+    public StudentDTO(LocalDate birthDate, String emailAddress, String firstName, String lastName, String userName, String password, String entry, Long facultyId, long alternateId, long applicantId, long studentId, String barCode) {
+        super(birthDate, emailAddress, firstName, lastName, userName,password);
+//        this.id = id;
+        this.entry = entry;
+        this.facultyId = facultyId;
+        this.alternateId = alternateId;
+        this.applicantId = applicantId;
+        this.studentId = studentId;
+        this.barCode = barCode;
+    }
 }

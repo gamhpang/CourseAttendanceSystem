16c43b4e39ea6fdbaad8cbbf0453a758096b7b3f
gamhpang <gamhpang1812@gmail.com>
2024-06-16 10:27:14 -0500

    fix optional for course get

--- src/main/java/cs544/courseattendancesystem/service/CourseService.java
+++ src/main/java/cs544/courseattendancesystem/service/CourseService.java
@@ -3,10 +3,11 @@ package cs544.courseattendancesystem.service;
 import cs544.courseattendancesystem.service.dto.CourseDTO;
 
 import java.util.Collection;
+import java.util.Optional;
 
 public interface CourseService {
     public CourseDTO createCourse(CourseDTO courseDTO);
-    public CourseDTO getCourse(long courseId);
+    public Optional<CourseDTO> getCourse(long courseId);
     public Collection<CourseDTO> getAllCourses();
     public CourseDTO updateCourse(long courseId,CourseDTO courseDTO);
     public void deleteCourse(long courseId);
